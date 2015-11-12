2b5a74261e0337d9736f3eaabf3574980bf7b233
nmirasch <neus.miras@gmail.com>
2015-11-12 15:49:54 +0100

    BZ:1261052 - Console: various localized perspective listings does not work

--- uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/ext/widgets/common/client/tables/ColumnPicker.java
+++ uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/ext/widgets/common/client/tables/ColumnPicker.java
@@ -16,12 +16,18 @@
 
 package org.uberfire.ext.widgets.common.client.tables;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.CheckBox;
 import com.github.gwtbootstrap.client.ui.DataGrid;
 import com.github.gwtbootstrap.client.ui.constants.IconType;
 import com.github.gwtbootstrap.client.ui.resources.ButtonSize;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -31,14 +37,7 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.apache.commons.lang3.StringUtils;
 import org.uberfire.ext.services.shared.preferences.GridColumnPreference;
 import org.uberfire.ext.services.shared.preferences.GridPreferencesStore;
 import org.uberfire.ext.widgets.common.client.resources.i18n.CommonConstants;
@@ -107,7 +106,7 @@ public class ColumnPicker<T> {
                 boolean found = false;
                 for (int i = 0; i < gridPreferences.getColumnPreferences().size() && !found; i++) {
                     GridColumnPreference gcp = gridPreferences.getColumnPreferences().get(i);
-                    if (gcp.getName().equals(columnMeta.getHeader().getValue())) {
+                    if (gcp.getName().equals(getColumnStoreName(columnMeta))) {
                         columnMeta.setVisible(true);
                         if (gcp.getWidth() != null) {
                             dataGrid.setColumnWidth(columnMeta.getColumn(), gcp.getWidth());
@@ -123,7 +122,7 @@ public class ColumnPicker<T> {
                     columnMeta.setVisible(false);
                 }
             } else if (gridPreferences.getGlobalPreferences() != null) {
-                int position = gridPreferences.getGlobalPreferences().getInitialColumns().indexOf(columnMeta.getHeader().getValue());
+                int position = gridPreferences.getGlobalPreferences().getInitialColumns().indexOf(getColumnStoreName(columnMeta));
                 if (position != -1) {
                     columnMeta.setVisible(true);
                     columnMeta.setPosition(position);
@@ -179,28 +178,27 @@ public class ColumnPicker<T> {
         VerticalPanel popupContent = new VerticalPanel();
 
         for (final ColumnMeta<T> columnMeta : columnMetaList) {
-
-            final CheckBox checkBox = new CheckBox(columnMeta.getHeader().getValue());
-
-            checkBox.setValue(columnMeta.isVisible());
-            checkBox.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-                public void onValueChange(ValueChangeEvent<Boolean> booleanValueChangeEvent) {
-                    boolean visible = booleanValueChangeEvent.getValue();
-                    if (visible) {
-                        dataGrid.insertColumn(getVisibleColumnIndex(columnMeta),
-                                columnMeta.getColumn(),
-                                columnMeta.getHeader());
-                    } else {
-                        dataGrid.removeColumn(columnMeta.getColumn());
-                    }
-                    columnMeta.setVisible(visible);
-                    adjustColumnWidths();
-                }
-            });
-
             if (gridPreferences == null || !gridPreferences.getGlobalPreferences()
-                    .getBannedColumns().contains(columnMeta.getHeader().getValue()))
+                    .getBannedColumns().contains(getColumnStoreName(columnMeta))) {
+                final CheckBox checkBox = new CheckBox(columnMeta.getHeader().getValue());
+                checkBox.setValue(columnMeta.isVisible());
+                checkBox.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+                    public void onValueChange(ValueChangeEvent<Boolean> booleanValueChangeEvent) {
+                        boolean visible = booleanValueChangeEvent.getValue();
+                        if (visible) {
+                            dataGrid.insertColumn(getVisibleColumnIndex(columnMeta),
+                                    columnMeta.getColumn(),
+                                    columnMeta.getHeader());
+                        } else {
+                            dataGrid.removeColumn(columnMeta.getColumn());
+                        }
+                        columnMeta.setVisible(visible);
+                        adjustColumnWidths();
+                    }
+                });
+
                 popupContent.add(checkBox);
+            }
         }
 
         if (gridPreferences != null) {
@@ -230,7 +228,7 @@ public class ColumnPicker<T> {
         }
 
         for (final ColumnMeta<T> columnMeta : columnMetaList) {
-            int position = gridPreferences.getGlobalPreferences().getInitialColumns().indexOf(columnMeta.getHeader().getValue());
+            int position = gridPreferences.getGlobalPreferences().getInitialColumns().indexOf(getColumnStoreName(columnMeta));
             columnMeta.setPosition(position);
             columnMeta.setVisible(position > -1);
         }
@@ -246,7 +244,7 @@ public class ColumnPicker<T> {
         List<GridColumnPreference> state = new ArrayList<GridColumnPreference>();
         for (final ColumnMeta<T> cm : columnMetaList) {
             if (cm.isVisible()) {
-                state.add(new GridColumnPreference(cm.getHeader().getValue(),
+                state.add(new GridColumnPreference(getColumnStoreName(cm),
                         dataGrid.getColumnIndex(cm.getColumn()),
                         dataGrid.getColumnWidth(cm.getColumn())));
             }
@@ -258,7 +256,7 @@ public class ColumnPicker<T> {
     private int getVisibleColumnIndex(final ColumnMeta<T> columnMeta) {
         int index = 0;
         for (final ColumnMeta<T> cm : columnMetaList) {
-            if (cm.equals(columnMeta)) {
+            if (cm.equals( columnMeta )) {
                 return index;
             }
             if (cm.isVisible()) {
@@ -308,12 +306,12 @@ public class ColumnPicker<T> {
 
             for ( ColumnMeta<T> cm : columnMetaList ) {
                 if (cm.isVisible()) {
-                    if ( columnsToCalculate.contains( cm.getHeader().getValue() ) ) {
+                    if ( columnsToCalculate.contains( getColumnStoreName(cm) ) ) {
                         dataGrid.setColumnWidth(cm.getColumn(),
                                 columnPCT,
                                 Style.Unit.PCT);
                     } else {
-                        dataGrid.setColumnWidth( cm.getColumn(), fixedWidths.get( cm.getHeader().getValue() ) );
+                        dataGrid.setColumnWidth( cm.getColumn(), fixedWidths.get( getColumnStoreName(cm) ) );
                     }
                 }
             }
@@ -357,5 +355,18 @@ public class ColumnPicker<T> {
         if (!columnInserted) {
             columnMetaList.add(columnMetaToMove);
         }
+    }
+
+    private String getColumnStoreName(ColumnMeta columnMeta){
+        if(columnMeta!=null){
+            if(columnMeta.getColumn()!=null) {
+                String colStoreName = columnMeta.getColumn().getDataStoreName();
+                if (colStoreName!=null && !colStoreName.isEmpty()) {
+                    return colStoreName;
+                }
+            }
+            return columnMeta.getCaption();
+        }
+        return "";
     }
 }
