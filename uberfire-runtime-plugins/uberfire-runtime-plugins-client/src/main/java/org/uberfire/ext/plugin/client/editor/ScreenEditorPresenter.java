9303fa712c5444cd4b10aaa5670069b5dba10355
Paulo Martins <paulovmr@gmail.com>
2016-09-01 15:16:31 -0300

    RHBPMS-933: [fr_FR, ru_RU] Need to translate a string on Dynamic menu editor page. RHBPMS-965: [ALL LANG] Unlocalized strings in screen Edit Component dialog. RHBPMS-1104: [de, fr] The tags page UI needs to be adjusted on the Extensions -> PlugIn Management page RHBPMS-1496: [ALL LANG] All items and dialogs under New drop-down menu and strings are unlocalized in Editor page of Plugins Explorer. RHBPMS-1591: [ALL LANG] Unlocalized strings in Perspective Editor. RHBPMS-1671: [pt_BR] '+Add Tag' button not fit in 'Add tag' dialog box.

--- uberfire-runtime-plugins/uberfire-runtime-plugins-client/src/main/java/org/uberfire/ext/plugin/client/editor/ScreenEditorPresenter.java
+++ uberfire-runtime-plugins/uberfire-runtime-plugins-client/src/main/java/org/uberfire/ext/plugin/client/editor/ScreenEditorPresenter.java
@@ -29,6 +29,7 @@ import org.uberfire.client.annotations.WorkbenchPartTitleDecoration;
 import org.uberfire.client.annotations.WorkbenchPartView;
 import org.uberfire.client.workbench.type.ClientResourceType;
 import org.uberfire.ext.editor.commons.client.file.SaveOperationService;
+import org.uberfire.ext.plugin.client.resources.i18n.CommonConstants;
 import org.uberfire.ext.plugin.client.type.ScreenPluginResourceType;
 import org.uberfire.ext.plugin.model.Media;
 import org.uberfire.ext.plugin.model.PluginContent;
@@ -70,7 +71,7 @@ public class ScreenEditorPresenter
 
     @WorkbenchPartTitle
     public String getTitleText() {
-        return "Screen PlugIn Editor [" + this.plugin.getName() + "]";
+        return CommonConstants.INSTANCE.ScreenPluginEditor() + " [" + this.plugin.getName() + "]";
     }
 
     @WorkbenchMenu
